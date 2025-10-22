7ee5f873dfccbd35c95d7770f9c943f35466b938
Muhammad Anaz Nazree Bin Sahidin <anaz.nazree@student.iium.edu.my>
2025-10-22 18:12:38 +0800

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,24 @@
+void main(){
+    var firstname = "Anaz";
+    var lastname = "Nazree";
+
+    int year = 2025;
+    int birthyear = 2003;
+    int age = year - birthyear;
+
+    double cgpa = 3.82;
+    String address = "Mahallah Bilal, 53000 Gombak, Selangor, Malaysia";
+
+    bool isStudent = true;
+    String status = "";
+    
+    if (isStudent == true){
+        status = "Student";
+    }
+    else {
+        status = "ex-Student";
+    }
+
+    print("My name is $firstname $lastname. I am $age this year. I am staying at $address. I am $status and my CGPA is $cgpa");
+
+}
